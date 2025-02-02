23005cbf911f06da75ce2de3e57719c234c2b9e9
Brandon Doza <141676838+BrandonDoza@users.noreply.github.com>
2025-02-02 13:55:09 -0700

    fix type

--- src/components/ui/color-mode.tsx
+++ src/components/ui/color-mode.tsx
@@ -32,7 +32,7 @@ export function ColorModeIcon() {
   return colorMode === "light" ? <LuSun /> : <LuMoon />;
 }
 
-interface ColorModeButtonProps extends Omit<IconButtonProps, "aria-label"> {}
+type ColorModeButtonProps = Omit<IconButtonProps, "aria-label">;
 
 export const ColorModeButton = React.forwardRef<
   HTMLButtonElement,
